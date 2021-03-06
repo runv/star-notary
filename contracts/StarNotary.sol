e840e9b09f5e584b9efb9d81b5d2062342afaa8d
natalir <nvrubinova@gmail.com>
2021-03-06 14:10:01 +0100

    test + feat: add basic tests
    Clean the project, add basic tests, add new function to change star name changeName + test for it

---
+++ contracts/StarNotary.sol
@@ -0,0 +1,23 @@
+pragma solidity >= 0.5.16;
+
+contract StarNotary {
+
+    string public starName;
+    address public starOwner;
+
+    event starClaimed(address owner);
+
+    constructor() public {
+        starName = "Awesome Udacity Star";
+    }
+
+    function claimStar() public {
+        starOwner = msg.sender;
+        emit starClaimed(msg.sender);
+    }
+
+    function changeName(string memory name) public {
+        starName = name;
+    }
+
+}
